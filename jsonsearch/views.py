5380aa2a08930fbb4488e8243fce4a0f96607cac
Never11n <never1n@ukr.net>
2023-11-27 15:24:11 +0200

    Update available version

--- jsonsearch/views.py
+++ jsonsearch/views.py
@@ -8,11 +8,11 @@ def code_search(request):
 
     product_code = []
     search_numbers = ['9', '3', '8', '2']
-    final_products = set()
+    final_products = []
     for product in data:
         product_code = str(product.get('productCode', ''))
         for number in search_numbers:
-            if str(number) in product_code:
-                final_products.add(product_code)
+            if str(number) in product_code and product not in final_products:
+                final_products.append(product)
 
     return render(request, 'main.html', {'final_products': final_products})
